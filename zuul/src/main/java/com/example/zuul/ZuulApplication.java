71e520b8895620ce724bf71a5e027ab8499ef4d8
ayrtoncarlos <bitao.metal@gmail.com>
2020-06-18 04:33:50 -0300

    Subindo alterações nos arquivos de configuração .yml

--- zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -3,11 +3,11 @@ package com.example.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulApplication 
 {
